746f77469516e1b667d60bc49e7f6f0ed1107e02
Sumit Gautam <141570316+CodeTimus@users.noreply.github.com>
2024-02-19 11:05:31 +0530

    first javascript

---
+++ section 1/javascript/Basics.js
@@ -0,0 +1,32 @@
+console.log("hello")
+console.log("sumit")
+console.log("hello")
+console.log("-----------")
+// differce bwt var and let
+var a=10;
+var a=20;
+// redeclearation is possible
+console.log(a)
+
+a=30;
+// reassingment is posible
+console.log(a)
+
+let b=10;
+// redeclearation is not posible
+// let b=20;
+// ressagnment is possible
+b=20;
+console.log(b)
+
+console.log("----------------")
+
+let firstname = "sumit"
+let lastname = "gautam"
+console.log(firstname + " " + lastname)
+
+let BS = 30000;
+let hra = (10/100)*BS;
+let da = (5/100)*BS;
+let gs = BS+ hra + da;
+console.log("your gross salary is " + gs)
---
+++ section 1/javascript/Datatypes.js
@@ -0,0 +1,24 @@
+// primitive Dataype
+// 1.Number
+// 2.String
+//3.boolean
+//4.null
+//5.undefined
+
+//non-primitibe Datatype
+//1.object
+//2.array
+//3.function
+
+let a= 20;
+console.log(typeof a)
+
+let b= "sumit"
+console.log(typeof b)
+
+let job;
+console.log(typeof job)
+
+
+let c=null;
+console.log(typeof c)
